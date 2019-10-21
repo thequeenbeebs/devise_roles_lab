50abb53b75e38657435a19a096b04f71cfa460fd
Steven G. Harms <steven.harms@flatironschool.com>
2019-10-21 12:16:54 -0400

    Passing User spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-
   after_validation :set_default_role
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  enum role: [:user, :vip, :admin]
 
   private
 
   def set_default_role
-    # Set default role here.
+    self.role ||= :user
   end
 end
